a436219ec1bd2e4abe1bcad850e92e5b940c86a6
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-09-14 07:59:05 +0530

    Update scripts.js

--- chapters/patterns/scripts.js
+++ chapters/patterns/scripts.js
@@ -2,5 +2,5 @@ function pattern(number) {
   if (number === 1) window.location.assign("Articles/pattern1.html");
   else if (number === 2) window.location.assign("Articles/pattern2.html");
   else if (number === 3) window.location.assign("Articles/pattern3.html");
-  else if (number === 4) window.location.assign("Articles/pattern4.html:);
+  else if (number === 4) window.location.assign("Articles/pattern4.html");
 }
