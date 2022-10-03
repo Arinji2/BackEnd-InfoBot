c6bafb0de824f23badf04012e8424cb8b467fd52
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-10-03 17:10:23 +0530

    Backend Update- Changed to Session Storage

--- chapters/methods/scripts.js
+++ chapters/methods/scripts.js
@@ -1,4 +1,13 @@
-const textBox = document.getElementById("input");
+//Storage
+
+let Topics = ["overloadvolume", "overloadcalculate", "firstcharinword"];
+let Links = [
+  "../methods/Articles/overloadvolume.html",
+  "../methods/Articles/overloadcalculate.html",
+  "../methods/Articles/firstcharinword.html",
+];
+
+let stringbox = document.getElementById("input");
 const choiceHead = document.getElementById("choiceHead");
 const choiceYes = document.getElementById("choiceYes");
 const choiceNo = document.getElementById("choiceNo");
@@ -10,27 +19,101 @@ choiceHead.style.display = "none";
 choiceYes.style.display = "none";
 choiceNo.style.display = "none";
 
-//Storage
+window.onload = function () {
+  sessionStorage.clear();
+};
 
-let Topics = ["overloadvolume", "overloadcalculate", "firstcharinword"];
-let Links = [
-  "../methods/Articles/overloadvolume.html",
-  "../methods/Articles/overloadcalculate.html",
-  "../methods/Articles/firstcharinword.html"
-];
+let word = "",
+  indexWord,
+  topicWord,
+  finalArrayOfWords = [];
 
-//Variables
-var indexWord;
-let textBoxValue;
-let length,
-  i,
-  j,
-  characters,
-  lengthArray,
-  word,
-  buttonNumber,
-  spaces = [0],
-  indexNum = 0;
+var checkWord;
+
+function getWordsArray(Stringbox) {
+  let arrayOfWords = [];
+  Stringbox = Stringbox + " ";
+  for (i = 0; i < Stringbox.length; i++) {
+    let letter = Stringbox.charAt(i);
+    if (letter != " ") {
+      word = word + letter;
+    } else if (letter == " ") {
+      arrayOfWords.push(word);
+      word = "";
+    }
+  }
+  return arrayOfWords;
+}
+
+function checkWordsArray(arrayOfWords) {
+  if (arrayOfWords.length == 0) showError();
+  for (i = 0; i < Topics.length; i++) {
+    topicWord = Topics[i];
+    for (j = 0; j < arrayOfWords.length; j++) {
+      if (topicWord == arrayOfWords[j]) finalArrayOfWords.push(topicWord);
+    }
+  }
+  return finalArrayOfWords;
+}
+function confirmation(number) {
+  let words = sessionStorage.getItem("Words");
+  console.log(words);
+  words = JSON.parse(words);
+  if (number == 3) {
+    if (typeof words[0] == "undefined") {
+      showError();
+      return;
+    }
+    choiceRender(1);
+    words[0] = toUpp(words[0]);
+    choiceHead.innerHTML =
+      "Did you Mean <span class='greenText'>" + words[0] + "</span>";
+  }
+  if (number == 1) {
+    for (i = 0; i < Topics.length; i++) {
+      if (Topics[i] == words[0]) window.location.assign(Links[i]);
+    }
+  }
+  if (number == 0) {
+    words.shift();
+    sessionStorage.removeItem("Words");
+    sessionStorage.setItem("Words", JSON.stringify(words));
+
+    confirmation(3);
+  }
+}
+
+function loading(number) {
+  if (number === 1)
+    output.innerHTML =
+      'Output: Searching <i class="white fa-spin fa-solid fa-spinner"></i>';
+  else if (number === 2) {
+    output.innerHTML =
+      'Output: Waiting for User Response <i class="white fa-spin fa-solid fa-spinner"></i>';
+    choiceRender(1);
+  }
+}
+function choiceRender(setting) {
+  if (setting == 1) {
+    choiceHead.style.display = "block";
+    choiceYes.style.display = "block";
+    choiceNo.style.display = "block";
+  } else if (setting == 2) {
+    choiceHead.style.display = "none";
+    choiceYes.style.display = "none";
+    choiceNo.style.display = "none";
+  }
+}
+
+function showError() {
+  console.log("Error");
+  choiceRender(2);
+  output.innerHTML =
+    ' <i class="red fa-solid fa-circle-xmark"></i> <br /> Output: Unfortunately We Could not locate your Question, Please Try Again with a different Question';
+  listPrograms();
+  return;
+}
+
 function toUpp(checkWord) {
   let checkLength = checkWord.length;
   let checkLetter;
@@ -43,93 +126,31 @@ function toUpp(checkWord) {
   return newWord;
 }
 
-function confirmation(buttonNumber) {
-  if (buttonNumber == 1) {
-    choiceRender(2);
-    window.location.assign(Links[indexWord]);
-  } else if (buttonNumber == 0) {
-    output.innerHTML =
-      ' <i class="red fa-solid fa-circle-xmark"></i> <br /> Output: Unfortunately We Could not locate your Question, Please Try Again with a different Question';
-    choiceRender(2);
-    listPrograms();
-  }
-}
-function loading(number) {
-  if (number === 1)
-    output.innerHTML =
-      'Output: Searching <i class="white fa-spin fa-solid fa-spinner"></i>';
-  else if (number === 2) {
-    output.innerHTML =
-      'Output: Waiting for User Response <i class="white fa-spin fa-solid fa-spinner"></i>';
-    choiceRender(1);
-  }
-}
-//prettier-ignore
-function main() {
-      let listDiv = document.getElementById("listItems");
-      listDiv.innerHTML = "";
-	let output = document.getElementById("output");
-	loading(1);
-    setTimeout(()=>{
-textBoxValue = textBox.value;
-textBoxValue = textBoxValue.toLowerCase();
-length = textBoxValue.length;
-for (i = 0; i < length; i++) {
-  characters = textBoxValue.charAt(i);
-  if (characters == " ") {
-    spaces.push(i);
-  }
-}
-
-lengthArray = spaces.length;
-for (i = 1; i <= lengthArray; i++) {
-  word = textBoxValue.substring(spaces[--i], spaces[++i]);
-  word = word.trim();
-  indexWord = Topics.indexOf(word);
-
-  if (indexWord !== -1) {
-    choiceRender(1);
-		loading(2);
-    word = toUpp(word);
-	console.log(word)
-    choiceHead.innerHTML = "Did You Mean " + word;
-  } 
-  if(indexWord == -1 && i == lengthArray){
-	  choiceRender(2);
-output.innerHTML =
-  ' <i class="red fa-solid fa-circle-xmark"></i> <br /> Output: Unfortunately We Could not locate your Question, Please Try Again with a different Question';
- listPrograms();
-
-
-
-}
-}
-	},2000)
-	
-}
-
-//prettier-ignore
-function choiceRender(setting) {
-	if(setting == 1){
-	choiceHead.style.display = "block";
-	choiceYes.style.display = "block";
-	choiceNo.style.display = "block";
-}else if(setting == 2){
-choiceHead.style.display = "none";
-choiceYes.style.display = "none";
-choiceNo.style.display = "none";
-}
-}
-
 function listPrograms() {
-  list.style.display = "block";
-  Topics.forEach((item) => {
-    item = toUpp(item);
-    indexNum++;
-    let listDiv = document.getElementById("listItems");
-    let text = document.createElement("li");
-    let content = document.createTextNode(item);
-    text.appendChild(content);
-    listDiv.appendChild(text);
-  });
+  if (list.style.display != "block") {
+    list.style.display = "block";
+    Topics.forEach((item) => {
+      item = toUpp(item);
+      let listDiv = document.getElementById("listItems");
+      let text = document.createElement("li");
+      let content = document.createTextNode(item);
+      text.appendChild(content);
+      listDiv.appendChild(text);
+    });
+  }
+}
+
+function main() {
+  sessionStorage.clear();
+  stringbox = document.getElementById("input");
+  finalArrayOfWords = [];
+  loading(1);
+  setTimeout(() => {
+    stringbox = stringbox.value;
+    let mainArrayOfWords = getWordsArray(stringbox);
+    let finalArrayOfWords = checkWordsArray(mainArrayOfWords);
+    sessionStorage.setItem("Words", JSON.stringify(finalArrayOfWords));
+    sessionStorage.setItem("Option", 0);
+    confirmation(3);
+  }, 2000);
 }
