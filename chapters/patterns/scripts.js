efbb8303939999dfc5bc1a632c14407f5873d455
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-10-03 20:25:20 +0530

    Remove Pattern 5

--- chapters/patterns/scripts.js
+++ chapters/patterns/scripts.js
@@ -3,5 +3,4 @@ function pattern(number) {
   else if (number === 2) window.location.assign("Articles/pattern2.html");
   else if (number === 3) window.location.assign("Articles/pattern3.html");
   else if (number === 4) window.location.assign("Articles/pattern4.html");
-  else if (number === 5) window.location.assign("Articles/MenuDrivenPattern.html");
 }
