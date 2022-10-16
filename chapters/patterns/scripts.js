9d0dfc9eeb7bb3d29afba91b26c8b6ea67128a3d
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-10-17 01:40:24 +0530

    Fix Pattern Programs

--- chapters/patterns/scripts.js
+++ chapters/patterns/scripts.js
@@ -3,4 +3,6 @@ function pattern(number) {
   else if (number === 2) window.location.assign("Articles/pattern2.html");
   else if (number === 3) window.location.assign("Articles/pattern3.html");
   else if (number === 4) window.location.assign("Articles/pattern4.html");
+  else if (number === 5) window.location.assign("Articles/pattern5.html");
+  else if (number === 6) window.location.assign("Articles/pattern6.html");
 }
