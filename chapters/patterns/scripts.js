a5a99257a3f68ddba34e794451df9b67352629a8
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-11-05 10:10:42 +0530

    Add all Patterns

--- chapters/patterns/scripts.js
+++ chapters/patterns/scripts.js
@@ -1,8 +1,34 @@
 function pattern(number) {
-  if (number === 1) window.location.assign("Articles/pattern1.html");
-  else if (number === 2) window.location.assign("Articles/pattern2.html");
-  else if (number === 3) window.location.assign("Articles/pattern3.html");
-  else if (number === 4) window.location.assign("Articles/pattern4.html");
-  else if (number === 5) window.location.assign("Articles/pattern5.html");
-  else if (number === 6) window.location.assign("Articles/pattern6.html");
+  switch (number) {
+    case 1:
+      window.location.assign("Articles/pattern1.html");
+      break;
+    case 2:
+      window.location.assign("Articles/pattern2.html");
+      break;
+    case 3:
+      window.location.assign("Articles/pattern3.html");
+      break;
+    case 4:
+      window.location.assign("Articles/pattern4.html");
+      break;
+    case 5:
+      window.location.assign("Articles/pattern5.html");
+      break;
+    case 6:
+      window.location.assign("Articles/pattern6.html");
+      break;
+    case 7:
+      window.location.assign("Articles/pattern7.html");
+      break;
+    case 8:
+      window.location.assign("Articles/pattern8.html");
+      break;
+    case 9:
+      window.location.assign("Articles/pattern9.html");
+      break;
+    case 10:
+      window.location.assign("Articles/pattern10.html");
+      break;
+  }
 }
